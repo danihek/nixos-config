160ad1cdcb47d5844419ba41e63bd900e7c0a689
danihek <danihek07@gmail.com>
2024-08-15 17:28:39 +0200

    config update

--- hosts/z690/configuration.nix
+++ hosts/z690/configuration.nix
@@ -188,7 +188,7 @@
     ckb-next # keyboard
     kdeconnect # watch and mobile
     obs-studio
-    android-tools gradle sdkmanager
+    android-tools gradle sdkmanager flutter
     markdown-oxide
     linuxPackages.usbip
     libreoffice-qt6-still
