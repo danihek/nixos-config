db03293ea173eb5393713b2bf4314714606d1f23
danihek <danihek07@gmail.com>
2024-08-16 18:08:28 +0200

    config update

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -3,12 +3,9 @@
 {
   programs.tmux = {
     enable = true;
-    shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "tmux-256color";
-    historyLimit = 100000;
-    plugins = with pkgs;
-      [
-        tmuxPlugins.better-mouse-mode
-      ];
+    extraConfig = ''
+      set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
+      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+    '';
   };
 }
