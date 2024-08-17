217ba58ca55eb3ac154bf64b38abbd68315bec4c
danihek <danihek07@gmail.com>
2024-08-17 16:38:08 +0200

    config update

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -34,12 +34,7 @@
 
     initExtra = ''
       autoload -U colors && colors
-
       autoload -Uz vcs_info
-      precmd_vcs_info() { vcs_info }
-      precmd_functions+=( precmd_vcs_info )
-      setopt prompt_subst
-      zstyle ':vcs_info:git:*' formats ":%b "
 
       # Prompt 
       PROMPT="[%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}] $fg[red]%}%$vcs_info_msg_0_"
