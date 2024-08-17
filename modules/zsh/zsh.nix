6ebbc1b394685d960507ea19d199cbaf7c2f14e0
danihek <danihek07@gmail.com>
2024-08-17 15:59:53 +0200

    nix config auto-commit

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -36,7 +36,7 @@
       autoload -U colors && colors
 
       # Prompt 
-      PROMPT="[%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}$(git_prompt_info)]{$vcs_info_msg_0_} "
+      PROMPT="[%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}$(git_prompt_info)] "
       zstyle ':vcs_info:git:*' formats ":%b"
 
       # Keys
