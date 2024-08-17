194fd342e2eabfad603f436440579c75842efd0f
danihek <danihek07@gmail.com>
2024-08-17 18:08:41 +0200

    config update

--- modules/foot/foot.nix
+++ modules/foot/foot.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, hostName, ...}:
+{ pkgs, config, ...}:
 
 {
   programs.foot = {
@@ -7,7 +7,7 @@
 
     settings = {
      main = {
-       include= "/home/${home.username}/.cache/wal/colors-foot.ini";
+       include= "/home/${config.users.users.username.home}/.cache/wal/colors-foot.ini";
        term = "xterm-256color";
        font = "JetBrainsMono Nerd Font:size=13";
        dpi-aware = "yes";
