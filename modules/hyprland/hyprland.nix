9ecdbafbc129f8c62786a91a2e627543367f1ee3
danihek <danihek07@gmail.com>
2024-08-18 19:02:14 +0200

    config update

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -149,6 +149,7 @@
         "$mod, 8, workspace, 8"  "$mod SHIFT, 8, movetoworkspace, 8"
         "$mod, 9, workspace, 9"  "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod, 0, workspace, 10" "$mod SHIFT, 0, movetoworkspace, 10"
+        "$mod, /, workspace, 11" "$mod SHIFT, /, movetoworkspace, 11"
 
         "$mod, mouse_down, workspace, e+1"
         "$mod, mouse_up, workspace, e-1"
