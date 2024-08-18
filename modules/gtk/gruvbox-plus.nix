533d3c5f7b2321fff0fcf401fa73ad87af9f4122
danihek <danihek07@gmail.com>
2024-08-18 23:10:14 +0200

    config update

--- modules/gtk/gruvbox-plus.nix
+++ modules/gtk/gruvbox-plus.nix
@@ -7,7 +7,9 @@ pkgs.stdenv.mkDerivation {
 
   src = pkgs.fetchurl {
     url = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v5.5.0/gruvbox-plus-icon-pack-5.5.0.zip";
+    sha256 = "0q9jrrncf56qkn5j918f7kx53253k9iy9jh5wacqvpwd17ipq7ni";
   };
+
   dontUnpack = true;
 
   installPhase = ''
