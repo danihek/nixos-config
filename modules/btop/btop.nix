5cbe19bcb74e64f0d700a8706313817f36aeb055
danihek <danihek07@gmail.com>
2024-08-18 11:06:20 +0200

    config update

--- modules/btop/btop.nix
+++ modules/btop/btop.nix
@@ -4,12 +4,12 @@
   programs = {
     btop = {
       enable = true;
-      package = pkgs.btop.overrideAttrs (oldAttrs: rec {
-        cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
-          "-DBTOP_RSMI_STATIC=ON"
-          "-DBTOP_GPU=ON"
-        ];
-      });
+     #package = pkgs.btop.overrideAttrs (oldAttrs: rec {
+     #  cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+     #    "-DBTOP_RSMI_STATIC=ON"
+     #    "-DBTOP_GPU=ON"
+     #  ];
+     #});
     };
   };
 }
