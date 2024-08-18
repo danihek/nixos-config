ad3a8f469974f5ac0dbdb3134ce1b605e2a97b94
danihek <danihek07@gmail.com>
2024-08-18 22:03:39 +0200

    config update

--- modules/gtk/gtk.nix
+++ modules/gtk/gtk.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, USERNAME, ... }:
 
+let
+  gsettings = "${pkgs.glib}/bin/gsettings";
+  gnomeSchema = "org.gnome.desktop.interface";
+  importGsettings = pkgs.writeShellScript "import_gsettings.sh" ''
+    config="/home/${USERNAME}/.config/gtk-3.0/settings.ini"
+    if [ ! -f "$config" ]; then exit 1; fi
+    gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+    icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+    cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+    font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+    ${gsettings} set ${gnomeSchema} gtk-theme "$gtk_theme"
+    ${gsettings} set ${gnomeSchema} icon-theme "$icon_theme"
+    ${gsettings} set ${gnomeSchema} cursor-theme "$cursor_theme"
+    ${gsettings} set ${gnomeSchema} font-name "$font_name"
+  '';
+in
 {
   dconf.settings = {
     "org/gnome/desktop/interface" = {
+      gtk-theme = "gruvbox-dark";
       color-scheme = "prefer-dark";
       cursor-size = 24;
       cursor-theme = "Numix-Cursor";
