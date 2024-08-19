303d0ccd9b1da5af4d97b59e806394048115efe0
danihek <danihek07@gmail.com>
2024-08-19 23:32:42 +0200

    config update

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -32,7 +32,7 @@
       setw -g aggressive-resize on
       setw -g monitor-activity on
 
-      bind C-l display-popup -E "~/code/bash/wls/wls.sh $(cat Makefecho ile | grep "PKGS " | cut -d'=' -f 2)"
+      bind l display-popup -E "~/code/bash/wls/wls.sh $(cat Makefile | grep "PKGS " | cut -d'=' -f 2)"
     '';
   };
 }
