fbd11d1f935d1ed9ce2cf956b7d05c5d9d37c32f
danihek <danihek07@gmail.com>
2024-08-19 17:21:29 +0200

    config update

--- modules/bash/bash.nix
+++ modules/bash/bash.nix
@@ -33,7 +33,7 @@
       # Prompt 
        GIT_PS1_CMD() { [ "$PS1_CMD1" != "" ] && PS1_CMD1="$PS1_CMD1 "; }
        PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>> /dev/null) ; GIT_PS1_CMD'
-       PS1='[\[\e[38;5;169;1m\]\u\[\e[0;38;5;196m\]@\[\e[38;5;69;1m\]\h\[\e[0m\]:\[\e[38;5;128m\]\w\[\e[0m\]] ''${PS1_CMD1}'
+       PS1='[\[\e[38;5;169;1m\]\u\[\e[0;38;5;196m\]@\[\e[38;5;69;1m\]\h\[\e[0m\]:\[\e[38;5;128m\]\w\[\e[0m\]]\e[38;5;156m\] ${PS1_CMD1}\[\e[0m\]'
     '';
     
     sessionVariables = {
