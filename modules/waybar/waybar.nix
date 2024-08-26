e2586e314496dd2a71c1be95106b905e6a4be660
danihek <danihek07@gmail.com>
2024-08-26 02:23:51 +0200

    config update

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -29,6 +29,7 @@
       "hyprland/workspaces" = {
         format = "{icon}";
         "all-outputs" = true;
+        "active-only" = false;
           format-icons = {
             "1" = "一";
             "2" = "二";
