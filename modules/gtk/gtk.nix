c16cadffcb67e0ab84666ccded1cd6fc550fd023
danihek <danihek07@gmail.com>
2024-08-26 23:14:28 +0200

    config update

--- modules/gtk/gtk.nix
+++ modules/gtk/gtk.nix
@@ -1,46 +1,6 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 24;
-  };
-
-  gtk = {
-    enable = true;
-    font = {
-      name = "Hack";
-      size = 12;
-      package = pkgs.hack-font;
-    };
-    theme = {
-      name = "Catppuccin-Macchiato-Compact-Blue-Dark";
-      package = pkgs.catppuccin-gtk;
-    };
-    iconTheme = {
-      name = "candy-icons";
-      package = pkgs.candy-icons;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme=1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme=1;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme = "gtk";
-    style = {
-        name = "adwaita-dark";
-        package = pkgs.adwaita-qt;
-    };
-  };
-
   dconf = {
     enable = true;
     settings = {
@@ -48,5 +8,34 @@
         color-scheme = "prefer-dark";
       };
     };
+  };
+
+  gtk = {
+    enable = true;
+
+    cursorTheme = {
+      name = "Catppuccin-Macchiato-Dark";
+      package = pkgs.catppuccin-cursors.macchiatoDark;
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Blue-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        size = "compact";
+        tweaks = [ "rimless" ];
+        variant = "macchiato";
+      };
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
   };
 }
