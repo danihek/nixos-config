26af199435b6eca9965ba36d260105c6a9d28065
danihek <danihek07@gmail.com>
2024-09-11 21:36:08 +0200

    config update

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -36,8 +36,7 @@
 
     colorscheme retrobox
 
-
-    imap <script><silent><nowait><expr> <C-g> codeium#Accept()
+    imap <C-g> codeium#Accept()
     imap <C-;>   <Cmd>call codeium#CycleCompletions(1)<CR>
     imap <C-,>   <Cmd>call codeium#CycleCompletions(-1)<CR>
     imap <C-x>   <Cmd>call codeium#Clear()<CR>
