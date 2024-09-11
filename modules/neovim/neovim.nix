f35260cebb70236a57e1da7777ba023f43b20a95
danihek <danihek07@gmail.com>
2024-09-11 21:41:29 +0200

    config update

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -35,8 +35,10 @@
     set nu rnu
 
     colorscheme retrobox
-
-    imap <C-g> codeium#Accept()
+    
+    <script><silent><nowait><expr> codeium#Disable()
+    vim.g.codeium_disable_bindings = 1
+    imap <script><silent><nowait><expr> <C-g> codeium#Accept()
     imap <C-;>   <Cmd>call codeium#CycleCompletions(1)<CR>
     imap <C-,>   <Cmd>call codeium#CycleCompletions(-1)<CR>
     imap <C-x>   <Cmd>call codeium#Clear()<CR>
