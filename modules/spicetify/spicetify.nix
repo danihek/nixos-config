18fdfcb127c86e867dc2f5dd069d229e757b1ec8
danihek <danihek07@gmail.com>
2024-09-15 14:03:43 +0200

    config update

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -1,28 +1,22 @@
-{ pkgs, inputs, ... }:
-
+{ pkgs, lib, inputs, ...}: 
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in 
 {
-  programs.spicetify =
-  let
-    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-  in
-  {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "spotify"
+    ];
+
+  imports = [inputs.spicetify-nix.homeManagerModules.default];
+
+  programs.spicetify = {
     enable = true;
-  
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
-    ];
-    enabledCustomApps = with spicePkgs.apps; [
-      newReleases
-      ncsVisualizer
-    ];
-    enabledSnippets = with spicePkgs.snippets; [
-      rotating-coverart
-      pointer
-    ];
-  
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+     enabledExtensions = with spicePkgs.extensions; [
+       adblock
+       shuffle+
+     ];
+     theme = spicePkgs.themes.dribbblish;
+     colorScheme = "gruvbox-material-dark";
   };
 }
