b359599841306477ffa9789693f3b27b7e74a67c
danihek <danihek07@gmail.com>
2024-09-15 13:55:56 +0200

    config update

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -173,6 +173,31 @@
     libsForQt5.qtstyleplugin-kvantum
   ];
 
+  programs.spicetify =
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in
+{
+  enable = true;
+
+  enabledExtensions = with spicePkgs.extensions; [
+    adblock
+    hidePodcasts
+    shuffle # shuffle+ (special characters are sanitized out of extension names)
+  ];
+  enabledCustomApps = with spicePkgs.apps; [
+    newReleases
+    ncsVisualizer
+  ];
+  enabledSnippets = with spicePkgs.snippets; [
+    rotating-coverart
+    pointer
+  ];
+
+  theme = spicePkgs.themes.catppuccin;
+  colorScheme = "mocha";
+}
+
   # Nix
   programs.nix-ld.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
