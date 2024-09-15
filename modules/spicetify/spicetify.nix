17cc2db6a5a1729765035ba5f2537b9dc2d25999
danihek <danihek07@gmail.com>
2024-09-15 13:57:03 +0200

    config update

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -1,26 +1,28 @@
 { pkgs, ... }:
 
-#programs.spicetify =
-#let
-#  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-#in
-#{
-#  enable = true;
-#
-#  enabledExtensions = with spicePkgs.extensions; [
-#    adblock
-#    hidePodcasts
-#    shuffle # shuffle+ (special characters are sanitized out of extension names)
-#  ];
-#  enabledCustomApps = with spicePkgs.apps; [
-#    newReleases
-#    ncsVisualizer
-#  ];
-#  enabledSnippets = with spicePkgs.snippets; [
-#    rotating-coverart
-#    pointer
-#  ];
-#
-#  theme = spicePkgs.themes.catppuccin;
-#  colorScheme = "mocha";
-#}
+{
+  programs.spicetify =
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in
+  {
+    enable = true;
+  
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
+    enabledCustomApps = with spicePkgs.apps; [
+      newReleases
+      ncsVisualizer
+    ];
+    enabledSnippets = with spicePkgs.snippets; [
+      rotating-coverart
+      pointer
+    ];
+  
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+  }
+}
