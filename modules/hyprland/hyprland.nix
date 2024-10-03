0db92f07703aa5d80f76a3d7d4f52ca1d61a0923
danihek <danihek07@gmail.com>
2024-10-03 20:04:22 +0200

    config update

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -37,7 +37,7 @@
       monitor = [
         "DP-1,2560x1440@165,0x50,1"
         "DP-2,2560x1440@165,2560x0,1,transform,0"
-        "$(hyprctl activeworkspace | grep "on monitor" | cut -d' ' -f 7 | sed 's/://g'),1920x1080@60,0x0,1" # my T430 with fhd ips B)
+        "LVDS-1,1920x1080@60,0x0,1" # my T430 with fhd ips B)
       ];
 
       "$terminal" = ''footclient -o "include=/home/${USERNAME}/.cache/wal/colors-foot.ini"'';
