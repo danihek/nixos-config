b1da42c0802d0061aede05ed56b3a119d9b33ce6
danihek <danihek07@gmail.com>
2024-10-03 22:37:47 +0200

    config update

--- modules/firefox/firefox.nix
+++ modules/firefox/firefox.nix
@@ -60,57 +60,57 @@ in
       };
 
       /* ---- POLICIES ---- */
-     #policies = {
-     #  DisableTelemetry = true;
-     #  DisableFirefoxStudies = true;
-     #  EnableTrackingProtection = {
-     #    Value = true;
-     #    Locked = true;
-     #    Cryptomining = true;
-     #    Fingerprinting = true;
-     #  };
-     #  DisablePocket = true;
-     #  DisableFirefoxAccounts = true;
-     #  DisableAccounts = true;
-     #  DisableFirefoxScreenshots = true;
-     #  OverrideFirstRunPage = "";
-     #  OverridePostUpdatePage = "";
-     #  DontCheckDefaultBrowser = true;
-     #  DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-     #  DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
-     #  SearchBar = "unified"; # alternative: "separate"
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified"; # alternative: "separate"
 
-     #  /* ---- EXTENSIONS ---- */
-     #  ExtensionSettings = {
-     #    "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-     #    "uBlock0@raymondhill.net" = {
-     #      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-     #      installation_mode = "force_installed";
-     #    };
-     #  };
+        /* ---- EXTENSIONS ---- */
+        ExtensionSettings = {
+          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
 
-     #  /* ---- PREFERENCES ---- */
-     #  Preferences = { 
-     #    "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-     #    "extensions.pocket.enabled" = lock-false;
-     #    "browser.formfill.enable" = lock-false;
-     #    "browser.search.suggest.enabled" = lock-false;
-     #    "browser.search.suggest.enabled.private" = lock-false;
-     #    "browser.urlbar.suggest.searches" = lock-false;
-     #    "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
-     #    "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-     #    "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-     #    "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-     #    "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-     #    "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-     #    "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-     #    "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-     #    "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-     #    "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-     #  };
-     #};
+        /* ---- PREFERENCES ---- */
+        Preferences = { 
+          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+          "extensions.pocket.enabled" = lock-false;
+          "browser.formfill.enable" = lock-false;
+          "browser.search.suggest.enabled" = lock-false;
+          "browser.search.suggest.enabled.private" = lock-false;
+          "browser.urlbar.suggest.searches" = lock-false;
+          "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        };
+      };
     };
   };
- #home.file."~/.mozilla/firefox/dh/user.js".source = "${shyFox}/user.js";
- #home.file."~/.mozilla/firefox/dh/chrome".source = "${shyFox}/chrome";
+  home.file."~/.mozilla/firefox/dh/user.js".source = "${shyFox}/user.js";
+  home.file."~/.mozilla/firefox/dh/chrome".source = "${shyFox}/chrome";
 }
