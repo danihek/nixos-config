26ae320f533c27e67ee1f0244e19b19a2cbe6dcd
danihek <danihek07@gmail.com>
2024-10-03 22:15:12 +0200

    config update

--- modules/firefox/firefox.nix
+++ modules/firefox/firefox.nix
@@ -21,7 +21,7 @@ in
     [Profile0]
     Name=default
     IsRelative=1
-    Path=danihek.default
+    Path=default
     Default=1
 
     [General]
@@ -30,18 +30,63 @@ in
   '';
 
   # Ensure that the default Firefox profile directory exists
-  home.file."~/.mozilla/firefox/danihek.default".directory = {
+  home.file."~/.mozilla/firefox/default".directory = {
     owner = USERNAME; # Replace with your actual username
   };
 
   # Copy the chrome folder and user.js from the shyFox repository to the profile directory
-  home.file."~/.mozilla/firefox/danihek.default/user.js".source = "${shyFox}/path/to/user.js"; # Adjust the path if necessary
-  home.file."~/.mozilla/firefox/danihek.default/chrome".source = "${shyFox}/path/to/chrome"; # Adjust the path if necessary
+  home.file."~/.mozilla/firefox/default/user.js".source = "${shyFox}/path/to/user.js"; # Adjust the path if necessary
+  home.file."~/.mozilla/firefox/default/chrome".source = "${shyFox}/path/to/chrome"; # Adjust the path if necessary
   
   programs = {
     firefox = {
       enable = true;
       languagePacks = [ "en-US" ];
+
+      /* PROFILES */
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+          settings = {
+            "browser.search.defaultenginename" = "Searx";
+            "browser.search.order.1" = "Searx";
+          };
+          search = {
+            force = true;
+            default = "Searx";
+            order = [ "Searx" "Google" ];
+            engines = {
+              "Nix Packages" = {
+                urls = [{
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
+              "NixOS Wiki" = {
+                urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@nw" ];
+              };
+              "Searx" = {
+                urls = [{ template = "https://searx.aicampground.com/?q={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@searx" ];
+              };
+              "Bing".metaData.hidden = true;
+              "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+            };
+          };
+        };
+      };
 
       /* ---- POLICIES ---- */
       policies = {
@@ -94,6 +139,34 @@ in
         };
       };
     };
+  };
+}
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.apps.firefox;
+in
+{
+  options.campground.apps.firefox = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Firefox.";
+    cac = mkBoolOpt false "Enable CAC Support";
+  };
+
+  config = mkIf cfg.enable {
+    # environment.systemPackages = with pkgs; [
+    #   nssTools
+    #   firefox
+    # ];
+
+    programs.firefox = {
+      enable = true;
+      ;
+    };
+    # # TODO: Add things to exploade cac certs and install them into firefox here
+    # campground.services.cac.enable = mkIf cfg.cac true;
   };
 }
 
