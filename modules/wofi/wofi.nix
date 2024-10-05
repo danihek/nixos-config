88a05b313acf1eab750e3b6bb4d0d6592c1fb6ec
danihek <danihek07@gmail.com>
2024-10-05 22:22:51 +0200

    config update

--- modules/wofi/wofi.nix
+++ modules/wofi/wofi.nix
@@ -5,11 +5,17 @@
       stylesheet = "~/.cache/wal/colors-wofi.css";
       allow_images = true;
       insensitiv = true;
-      no_actions = true;
       columns = 5;
-      show = "drun";
-      term = "foot";
-      layer = "overlay";
+      width = 600;
+      height = 400;
+      mode = drun;
+      filter_rate = 100;
+      allow_markup = true;
+      no_actions = true;
+      halign = fill;
+      orientation = vertical;
+      content_halign = fill;
+      drun-display_generic = true;
     };
   };
 }
