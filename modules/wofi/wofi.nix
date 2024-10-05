dc76c8f47d3eeb8dfd9291d6f1885c3c6e1aed9a
danihek <danihek07@gmail.com>
2024-10-05 21:36:24 +0200

    config update

--- modules/wofi/wofi.nix
+++ modules/wofi/wofi.nix
@@ -1,19 +1,49 @@
-{ config, pkgs, ... }:
-
 {
-
   programs.wofi = {
     enable = true;
-
     settings = {
-      allow_images = true;
-      insensitiv = true;
-      no_actions = true;
-      columns = 2;
-      show = "drun";
+      width = 500;
+      mode = "run";
+      stylesheet = "~/.cache/wal/colors-wofi.css";
+      prompt = "";
+      filter_rate = "200";
+      insensitve = true;
+      gtk_dark = true;
+      term = "foot";
+      layer = "overlay";
     };
-    style = ''
-      @import url('~/.cache/wal/colors-wofi.css')
-    '';
+#   style = ''
+#     window {
+#     	background-color: rgba(--wofi-rgb-color0,0.85);
+#     	font-family: "Iosevka Nerd Font";
+#     	transition: 0.1s ease-in-out;
+#     	font-size: 140%;
+#     }
+#     #input image {
+#     	color: rgba(0,0,0,0)
+#     }
+#     #input {
+#     	margin-bottom: 20px;
+#     	margin-left: 100px;
+#     	margin-right: 100px;
+#     	margin-top: 15px;
+#     	border: 3px solid --wofi-color2;
+#     }
+#     label {
+#     	color: --wofi-color7;
+#     	transition: 0.1s ease-in-out;
+#     }
+#     #entry {
+#     	padding: 5px;
+#     	padding-left: 15px;
+#     }
+#     #entry:selected {
+#     	background-color: --wofi-color2;
+#     }
+#     #outer-box {
+#     	border: 4px solid --wofi-color2;
+#     	transition: 0.1s ease-in-out;
+#     }
+#   '';
   };
 }
