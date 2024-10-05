a47aca54eb66fd0f68306aae111008a5a4fe0262
danihek <danihek07@gmail.com>
2024-10-05 21:09:29 +0200

    config update

--- modules/rofi/rofi.nix
+++ modules/rofi/rofi.nix
@@ -17,25 +17,13 @@
     theme = "${config.xdg.cacheHome}/wal/colors-rofi-light.rasi";
 
     extraConfig = {
-      show-icons = true;           # Enable icons
-      element-icon = true;         # Ensure icons are displayed
-      element-icon-size = "3.0ch"; # Increase the icon size
-
-      scrollbar = false;           # Disable scrollbar for a minimal look
-
-      # Grid layout with larger icons and more space between elements
-      columns = 3; # 3-column grid layout
-      lines = 5;   # Show 5 lines per page, you can increase this
-
-      # Custom prompt text for personalization
-      prompt = "Choose your action 💻: ";
-
-      # Padding and spacing for better layout
-      padding = "10px 20px";
-      spacing = "10px";
-
-      # Custom line height to space out elements
-      line-height = "1.8";
+      show-icons = true;
+      icon-theme = "Papirus-Apps";
+      terminal = "foot";
+      cycle = false;
+      hide-scrollbar = true;
+      disable-history = false;
+      opacity = 0.5;
     };
 
     plugins = with pkgs; [
