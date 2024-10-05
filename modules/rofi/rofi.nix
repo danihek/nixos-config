5dbafe5b388f30cabbdbb3059641afb3c8154976
danihek <danihek07@gmail.com>
2024-10-05 20:54:09 +0200

    config update

--- modules/rofi/rofi.nix
+++ modules/rofi/rofi.nix
@@ -7,18 +7,17 @@
   {
     enable = true;
     cycle = true;
-    # Install the wayland variant of rofi
     package = pkgs.rofi-wayland;
-    # Set terminal to kitty
     terminal = "${pkgs.foot}/bin/foot";
 
     font = "MesloLGS Nerd Font Mono 10";
     theme = "${config.xdg.cacheHome}/wal/colors-rofi-light.rasi";
 
     extraConfig = {
-      element-icon = { size = 1.65; };
+      element-icon = { size = 2.5; };
       show-icons = true;
       scrollbar = false;
+      dpi = 192;
     };
     plugins = with pkgs; [
       rofi-emoji
