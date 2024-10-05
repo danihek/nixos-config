077e335e09eb2ae95ae8e94d036bc4728c2f407d
danihek <danihek07@gmail.com>
2024-10-05 15:48:13 +0200

    config update

--- modules/rofi/rofi.nix
+++ modules/rofi/rofi.nix
@@ -3,8 +3,6 @@
 {
   programs.rofi = {
         enable = true;
-        package = cfg.package;
-        plugins = [ pkgs.rofi-power-menu ];
   
         extraConfig = {
           disable-history = false;
@@ -15,13 +13,11 @@
           drun-display-format = "{icon} {name}";
           display-drun = "   Run ";
           display-window = " 﩯 Window ";
-          display-power-menu = "  Power Menu ";
   
           modi = concatStringsSep "," [
             "run"
             "drun"
             "filebrowser"
-            "power-menu"
           ];
   
           xoffset = 0;
