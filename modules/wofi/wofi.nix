e755c029f7bdb8ff5469ace60723f635ef7b7595
danihek <danihek07@gmail.com>
2024-10-12 22:00:32 +0200

    config update

--- modules/wofi/wofi.nix
+++ modules/wofi/wofi.nix
@@ -2,7 +2,6 @@
   programs.wofi = {
     enable = true;
     settings = {
-      stylesheet = "~/.cache/wal/colors-wofi.css";
       allow_images = true;
       insensitiv = true;
       columns = "5";
