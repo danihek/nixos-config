4e946128f6a6c0b346eaf6598d6e2e4081da8a27
danihek <danihek07@gmail.com>
2024-10-19 22:46:16 +0200

    config update

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -3,14 +3,38 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;
-    extraConfig = ''
-      (setq standard-indent 2)
-    '';
+    package = pkgs.emacs26-nox;
+    extraPackages = (
+      epkgs:
+      [
+        epkgs.melpaPackages.use-package
+
+        epkgs.melpaPackages.cargo
+        epkgs.melpaPackages.flycheck
+        epkgs.melpaPackages.flymake-cursor
+        epkgs.melpaPackages.magit
+        epkgs.melpaPackages.smex
+        epkgs.melpaPackages.rainbow-delimiters
+
+        epkgs.melpaPackages.markdown-mode
+        epkgs.melpaPackages.rust-mode
+        epkgs.melpaPackages.toml-mode
+        epkgs.melpaPackages.web-mode
+        epkgs.melpaPackages.yaml-mode
+       ]
+      );
   };
 
-  services.emacs = {
-    enable = false;
-    package = pkgs.emacs;
-  };
+  home.file.".emacs.d/init.el" = ''
+    (scroll-bar-mode -1)
+    (menu-bar-mode -1)
+    (tool-bar-mode -1)
+    (global-display-line-numbers-mode)
+    
+    (ido-mode 1)
+    (ido-everywhere 1)
+    
+    (custom-set-variables
+     '(display-line-numbers-type (quote relative)))
+    '';
 }
