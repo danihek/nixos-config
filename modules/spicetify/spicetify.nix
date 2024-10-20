275c9105c75ac37251d70e4eb480d377df409d0d
danihek <danihek07@gmail.com>
2024-10-20 15:47:26 +0200

    config update

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -17,7 +17,7 @@ in
        hidePodcasts
        shuffle # shuffle+ (special characters are sanitized out of extension names)
      ];
-     theme = spicePkgs.themes.spicetify-dynamic-theme;
+     theme = spicePkgs.themes.DefaultDynamic;
      colorScheme = "DefaultDynamic";
   };
 }
