333f3c64cb86fbda0c513c72f29ce5995d7e623e
danihek <danihek07@gmail.com>
2024-10-20 15:47:48 +0200

    config update

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -17,7 +17,7 @@ in
        hidePodcasts
        shuffle # shuffle+ (special characters are sanitized out of extension names)
      ];
-     theme = spicePkgs.themes.DefaultDynamic;
+     theme = spicePkgs.themes.defaultDynamic;
      colorScheme = "DefaultDynamic";
   };
 }
