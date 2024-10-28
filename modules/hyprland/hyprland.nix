a6b4b61e23576f1413e5609701a816223256be62
danihek <danihek07@gmail.com>
2024-10-28 22:22:24 +0100

    config update

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -41,7 +41,7 @@
       ];
 
       "$terminal" = ''footclient -o "include=/home/${USERNAME}/.cache/wal/colors-foot.ini"'';
-      "$menu" = "echo \"Do me senpai\" | wofi --show drun";
+      "$menu" = "rofi -show drun";
       "$lock" = "hyprlock";
 
       general = {
