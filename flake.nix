4c99e97da05e8f526610288c0cd4f8f56966b4c1
danihek <danihek07@gmail.com>
2024-11-01 00:29:53 +0100

    config update

--- flake.nix
+++ flake.nix
@@ -69,12 +69,7 @@
           ./hosts/shared.nix
           ./hosts/z690/configuration.nix
           inputs.home-manager.nixosModules.default
-
-          ({ config, pkgs, ... }: {
-           environment.systemPackages = with pkgs; [
-              themecord.packages.x86_64-linux.default
-           ];
-          })
+          themecord.packages.x86_64-linux.default
           
           ({ config, pkgs, ... }: {
             environment.systemPackages = with pkgs; [
