b882cf4f591bc06ddbdb3ed19ac644f0bbd12022
danihek <danihek07@gmail.com>
2024-11-08 23:12:59 +0100

    config update

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -26,26 +26,11 @@
       nvim-autopairs
       nvim-lspconfig
       vim-unimpaired
+      nvim-treesitter.c3
       nvim-treesitter.withAllGrammars
     ];
 
     extraLuaConfig = ''
-        vim.filetype.add({
-          extension = {
-            c3 = "c3",
-            c3i = "c3",
-            c3t = "c3",
-          },
-        })
-        
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.c3 = {
-          install_info = {
-            url = "https://github.com/c3lang/tree-sitter-c3",
-            files = {"src/parser.c", "src/scanner.c"},
-            branch = "main",
-          },
-        }
         '';
 
     extraConfig = ''
