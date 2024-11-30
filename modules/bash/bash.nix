e34dea50de3d9600e57aeb6ff92000be6649cb99
danihek <danihek07@gmail.com>
2024-11-30 01:48:19 +0100

    config update

--- modules/bash/bash.nix
+++ modules/bash/bash.nix
@@ -54,6 +54,9 @@
       # Keybindings
       bind '"\e[1;5A": history-search-backward'
       bind '"\e[1;5B": history-search-forward'
+
+      source ~/.cache/hellwal/variables.sh
+      sh ~/.cache/hellwal/terminal.sh
     '';
     
     sessionVariables = {
