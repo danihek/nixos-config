1ff80a94aaaeb270b494ab9952644509e18681a1
danihek <danihek07@gmail.com>
2024-12-02 22:15:51 +0100

    config update

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 
+let
+  vim-better-whitespace = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-better-whitespace";
+    src = pkgs.fetchFromGitHub {
+      owner = "ntpeters";
+      repo = "vim-better-whitespace";
+      rev = "984c8da518799a6bfb8214e1acdcfd10f5f1eed7";
+      sha256 = "10l01a8xaivz6n01x6hzfx7gd0igd0wcf9ril0sllqzbq7yx2bbk";
+    };
+  };
+
+in 
 {
   programs.neovim = {
     enable = true;
