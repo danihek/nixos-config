05b9038c91b793dbb9fd02ce8da8767c98c20be8
danihek <danihek07@gmail.com>
2024-12-07 15:56:08 +0100

    config update

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -157,7 +157,7 @@ window#waybar {
 	color: @background;
 }
 
-#custom music {
+#custom-music {
     color: @foreground;
     padding: 0 8px;
     background-color: @background;
@@ -165,7 +165,7 @@ window#waybar {
     transition: background-color 0.3s, color 0.3s;
 }
 
-#custom music:hover {
+#custom-music:hover {
     background-color: @color7;
     color: @color15;
 }
