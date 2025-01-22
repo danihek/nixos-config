7d50656feab777b519d1c385d5cc404b39f7039f
Daniel <danihek07@gmail.com>
2025-01-22 01:20:43 +0100

    config update

--- hosts/t430/configuration.nix
+++ hosts/t430/configuration.nix
@@ -80,7 +80,6 @@ in
   };
 
   # Bluetooth
-  services.bluetooth.enable = true;
   services.blueman.enable = true;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = false; # powers up the default Bluetooth controller on boot
