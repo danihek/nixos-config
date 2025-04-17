e5a402df47f56ab2ba04be51279ba294719544dd
danihek <danihek07@gmail.com>
2025-04-17 19:39:35 +0200

    config update

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -18,6 +18,5 @@ in
        shuffle # shuffle+ (special characters are sanitized out of extension names)
      ];
      theme = spicePkgs.themes.defaultDynamic;
-     colorScheme = "defaultDynamic";
   };
 }
