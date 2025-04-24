61f81e475d07e4da16c3284f1edfdc8e3ac1a27d
danihek <danihek07@gmail.com>
2025-04-24 19:59:20 +0200

    config update

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -9,7 +9,7 @@
     defaultEditor = true;
 
     plugins = with pkgs.vimPlugins; [
-      vim-multiple-cursors
+      vim-visual-multi
       sonokai
       pywal-nvim
       dracula-vim
@@ -132,19 +132,6 @@
 
       inoremap <silent><expr> <cr> "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
       inoremap <silent><expr> <tab> coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<tab>"
-
-      "Vim multiple cursors
-      let g:multi_cursor_use_default_mapping=0
-
-      " Default mapping
-      let g:multi_cursor_start_word_key      = '<C-n>'
-      let g:multi_cursor_select_all_word_key = '<A-n>'
-      let g:multi_cursor_start_key           = 'g<C-n>'
-      let g:multi_cursor_select_all_key      = 'g<A-n>'
-      let g:multi_cursor_next_key            = '<C-n>'
-      let g:multi_cursor_prev_key            = '<C-p>'
-      let g:multi_cursor_skip_key            = '<C-x>'
-      let g:multi_cursor_quit_key            = '<Esc>'
 '';
     };
 
